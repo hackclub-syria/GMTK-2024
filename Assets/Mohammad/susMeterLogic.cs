3af83ad3cd0d6b84d77d5ab23e6d558d049b860f
Sandouk <111926705+simasandouk@users.noreply.github.com>
2024-08-19 18:26:54 +0300

    Added Stat Screen UI to main game scene
    (bonus assets included)

--- Assets/Mohammad/susMeterLogic.cs
+++ Assets/Mohammad/susMeterLogic.cs
@@ -9,25 +9,28 @@ public class susMeterLogic : MonoBehaviour
     public Slider susSlider;
     public float crowdIQ;
     public float susValue;
-    
-   public void changeSusValue(float susChange){
-    susValue += susChange;
-    susSlider.value = susValue;
-   }
+
+    public void changeSusValue(float susChange)
+    {
+        susValue += susChange;
+        susSlider.value = susValue;
+    }
 
 
 
-   public float susAmmoutOfChange(float initialHoleSize, float currentHoleSize){
-    float value;
-    value = Math.Abs((currentHoleSize-0.5f) + (currentHoleSize- currentHoleSize) )/2;
-    return value*crowdIQ;
-   }
+    public float susAmmoutOfChange(float initialHoleSize, float currentHoleSize)
+    {
+        float value;
+        value = Math.Abs((currentHoleSize - 0.5f) + (currentHoleSize - currentHoleSize)) / 2;
+        return value * crowdIQ;
+    }
 
 
 
-   public void susMeter(float initialHoleSize, float currentHoleSize ){
-    float susChange = susAmmoutOfChange(initialHoleSize , currentHoleSize);
-    changeSusValue(susChange);
-     
-   }
+    public void susMeter(float initialHoleSize, float currentHoleSize)
+    {
+        float susChange = susAmmoutOfChange(initialHoleSize, currentHoleSize);
+        changeSusValue(susChange);
+
+    }
 }
