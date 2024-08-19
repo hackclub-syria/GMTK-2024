4d4db5ae1e2a3aae5f1ed0ca0ee9231e93173c60
Sandouk <111926705+simasandouk@users.noreply.github.com>
2024-08-19 19:38:40 +0300

    Manual main to Sima merge

--- Assets/Mohammad/susMeterLogic.cs
+++ Assets/Mohammad/susMeterLogic.cs
@@ -1,10 +1,3 @@
-// each time susMeter is called it chnges the value of the susSlider and susValue 
-// to change crowds sensitivity for change change crowdIQ
-
-
-
-
-
 using System.Collections;
 using System;
 using System.Collections.Generic;
@@ -14,27 +7,30 @@ using UnityEngine.UI;
 public class susMeterLogic : MonoBehaviour
 {
     public Slider susSlider;
-    public float crowdIQ = 0.5f;
-    public float susValue = 0;
-    
-   public void changeSusValue(float susChange){
-    susValue += susChange;
-    susSlider.value = susValue;
-   }
+    public float crowdIQ;
+    public float susValue;
+
+    public void changeSusValue(float susChange)
+    {
+        susValue += susChange;
+        susSlider.value = susValue;
+    }
 
 
 
-   public float susAmmoutOfChange(float initialHoleSize, float currentHoleSize){
-    float value;
-    value = Math.Abs((currentHoleSize-0.5f) + (currentHoleSize- currentHoleSize) )/2;
-    return value*crowdIQ;
-   }
+    public float susAmmoutOfChange(float initialHoleSize, float currentHoleSize)
+    {
+        float value;
+        value = Math.Abs((currentHoleSize - 0.5f) + (currentHoleSize - currentHoleSize)) / 2;
+        return value * crowdIQ;
+    }
 
 
 
-   public void susMeter(float initialHoleSize, float currentHoleSize ){
-    float susChange = susAmmoutOfChange(initialHoleSize , currentHoleSize);
-    changeSusValue(susChange);
-     
-   }
+    public void susMeter(float initialHoleSize, float currentHoleSize)
+    {
+        float susChange = susAmmoutOfChange(initialHoleSize, currentHoleSize);
+        changeSusValue(susChange);
+
+    }
 }
