2ae4f734dd770680caa9c9bd2c7095312d9636c8
Sandouk <111926705+simasandouk@users.noreply.github.com>
2024-08-19 19:24:32 +0300

    git decided to have files that changed so here we are

--- Assets/Mohammad/susMeterLogic.cs
+++ Assets/Mohammad/susMeterLogic.cs
@@ -9,28 +9,25 @@ public class susMeterLogic : MonoBehaviour
     public Slider susSlider;
     public float crowdIQ;
     public float susValue;
-
-    public void changeSusValue(float susChange)
-    {
-        susValue += susChange;
-        susSlider.value = susValue;
-    }
-
-
-
-    public float susAmmoutOfChange(float initialHoleSize, float currentHoleSize)
-    {
-        float value;
-        value = Math.Abs((currentHoleSize - 0.5f) + (currentHoleSize - currentHoleSize)) / 2;
-        return value * crowdIQ;
-    }
+    
+   public void changeSusValue(float susChange){
+    susValue += susChange;
+    susSlider.value = susValue;
+   }
 
 
 
-    public void susMeter(float initialHoleSize, float currentHoleSize)
-    {
-        float susChange = susAmmoutOfChange(initialHoleSize, currentHoleSize);
-        changeSusValue(susChange);
+   public float susAmmoutOfChange(float initialHoleSize, float currentHoleSize){
+    float value;
+    value = Math.Abs((currentHoleSize-0.5f) + (currentHoleSize- currentHoleSize) )/2;
+    return value*crowdIQ;
+   }
 
-    }
+
+
+   public void susMeter(float initialHoleSize, float currentHoleSize ){
+    float susChange = susAmmoutOfChange(initialHoleSize , currentHoleSize);
+    changeSusValue(susChange);
+     
+   }
 }
