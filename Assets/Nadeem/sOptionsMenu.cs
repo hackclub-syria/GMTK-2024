ac08b0483b601cf458046d9c9e0c0260892f964b
Gontoblez <127008794+gontoblez@users.noreply.github.com>
2024-08-19 03:57:47 +0300

    Created Volume from Main Menu Scene
    Everything is working as expected

--- Assets/Nadeem/sOptionsMenu.cs
+++ Assets/Nadeem/sOptionsMenu.cs
@@ -12,37 +12,6 @@ public class sOptionsMenu : MonoBehaviour
     public GameObject MainMenu;
     public GameObject OptionsMenu;
     
-    // VOLUME VARIABLES
-    public TextMeshProUGUI masterVol;
-    public TextMeshProUGUI SFXVol;
-    public TextMeshProUGUI CommVol;
-    public TextMeshProUGUI musicVol;
-
-    // BEGINNING OF VOLUME STUFF
-    public void setMasterVol(float volume)
-    {
-        volume = (80 + volume) / 80 * 100;
-        masterVol.text = Math.Round(volume).ToString() + "%";
-    }
-
-    public void setSFXVol(float volume)
-    {
-        volume = (80 + volume) / 80 * 100;
-        SFXVol.text = Math.Round(volume).ToString() + "%";
-    }
-    public void setCommVol(float volume)
-    {
-        volume = (80 + volume) / 80 * 100;
-        CommVol.text = Math.Round(volume).ToString() + "%";
-    }
-    
-    public void setMusicVol(float volume)
-    {
-        volume = (80 + volume) / 80 * 100;
-        musicVol.text = Math.Round(volume).ToString() + "%";
-    }
-    // END OF VOLUME STUFF
-
     // Set full screen
     public void setFullscreen(bool fullscreen)
     {
